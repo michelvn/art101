21489b337237a2d629debbc9df347f5dbfc19392
Michel <60534295+michelvn@users.noreply.github.com>
2021-11-26 23:13:25 -0800

    updated basic page for lab 5

---
+++ lab5/js/lab.js
@@ -0,0 +1,20 @@
+// Michel Nguyen
+// ART 101, Lab 5: Data Types and Variables
+// Wes Modes, fall 2021
+
+// Create variables to hold data about your car.
+make = "Lexus";
+model = "LS 430";
+color = "white";
+year = 2005;
+ownIt = Boolean(0);
+
+// Calculate the age of the car.
+age = 2021 - year;
+
+// Print the variables.
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");

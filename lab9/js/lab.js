bf78b5f6f964dc8e85d3d7648f5aef12e3c04703
Michel <60534295+michelvn@users.noreply.github.com>
2021-11-27 06:30:51 -0800

    completed lab 9

---
+++ lab9/js/lab.js
@@ -0,0 +1,19 @@
+// Michel Nguyen
+// ART 101, Lab 5: Data Types and Variables
+// Wes Modes, fall 2021
+
+outputEl = document.getElementById("output");
+
+new1El = document.createElement("p");
+new1El.id = "newElement1";
+new1El.innerHTML = "Something new here!";
+
+new2El = document.createElement("p");
+new2El.id = "newElement2";
+new2El.innerHTML = "Something else new!";
+
+outputEl.appendChild(new1El);
+outputEl.appendChild(new2El);
+
+outputEl.style.backgroundColor = "#F8D9FC";
+outputEl.style.color = "black";

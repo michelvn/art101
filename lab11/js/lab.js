711d4b89ec8a62428ad8b6c1a3a98f84f3909ce8
Michel <60534295+michelvn@users.noreply.github.com>
2021-11-28 07:21:36 -0800

    created buttons for lab 11

---
+++ lab11/js/lab.js
@@ -0,0 +1,9 @@
+// Michel Nguyen
+// ART 101, Lab 11: Libraries and jQuery
+// Wes Modes, fall 2021
+
+$("#challenge").html('<button type="button">Click Me!</button>');
+
+$("#problems").html('<button type="button">Click Me!</button>');
+
+$("#output").html('<button type="button">Click Me!</button>');

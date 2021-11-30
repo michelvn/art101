d7d5977daa7011c909fa20623757207276769f1a
Michel <60534295+michelvn@users.noreply.github.com>
2021-11-30 15:43:10 -0800

    added custom menu button images

--- lab11/js/lab.js
+++ lab11/js/lab.js
@@ -2,8 +2,14 @@
 // ART 101, Lab 11: Libraries and jQuery
 // Wes Modes, fall 2021
 
-$("#challenge").html('<button type="button">Click Me!</button>');
+$("#challenge").html('<button type="button" class="red">Click Me!</button>');
+$("button.red").click(function(){
+  // remove possible existing class
+  $("#output").removeClass("green");
+  // now toggle red class
+  $("#output").toggleClass("red");
 
+}
 $("#problems").html('<button type="button">Click Me!</button>');
 
 $("#output").html('<button type="button">Click Me!</button>');

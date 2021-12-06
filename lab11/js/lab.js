e1379071e105d304a07b4ab8c3e9e0917882426c
Michel <60534295+michelvn@users.noreply.github.com>
2021-12-06 06:20:02 -0800

    migrated progress from lab 14

--- lab11/js/lab.js
+++ lab11/js/lab.js
@@ -2,14 +2,19 @@
 // ART 101, Lab 11: Libraries and jQuery
 // Wes Modes, fall 2021
 
-$("#challenge").html('<button type="button" class="red">Click Me!</button>');
-$("button.red").click(function(){
-  // remove possible existing class
-  $("#output").removeClass("green");
-  // now toggle red class
-  $("#output").toggleClass("red");
+function help() {
+  challengeEl = document.getElementById("challenge");
+  challengeEl.classList.toggle("special");
+};
+// button.addEventListener("click", function() {
+//    targetEl.innerHTML = "Fuck";
+//  });
+function help2() {
+  problemEl = document.getElementById("problem");
+  problemEl.classList.toggle("special");
+};
 
-}
-$("#problems").html('<button type="button">Click Me!</button>');
-
-$("#output").html('<button type="button">Click Me!</button>');
+function help3() {
+  resultsEl = document.getElementById("results");
+  resultsEl.classList.toggle("special");
+};

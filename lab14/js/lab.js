723ada6f1d72028ce08ea910a737f5027d8c4b8f
Michel <60534295+michelvn@users.noreply.github.com>
2021-12-06 06:17:44 -0800

    working buttons for lab 11

---
+++ lab14/js/lab.js
@@ -0,0 +1,20 @@
+// Michel Nguyen
+// ART 101, Lab
+// Wes Modes, fall 2021
+
+function help() {
+  challengeEl = document.getElementById("challenge");
+  challengeEl.classList.toggle("special");
+};
+// button.addEventListener("click", function() {
+//    targetEl.innerHTML = "Fuck";
+//  });
+function help2() {
+  problemEl = document.getElementById("problem");
+  problemEl.classList.toggle("special");
+};
+
+function help3() {
+  resultsEl = document.getElementById("results");
+  resultsEl.classList.toggle("special");
+};
